b57b191c79dde004b470ed03f867516cac5f35df
luiscoding <lu.zhang.code@gmail.com>
2020-04-01 18:45:39 -0400

    nationality &place

---
+++ run_nationality_train.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+relations=(
+# "people@person@place_of_birth"
+  "people@person@nationality"
+# "film@film@language"
+)
+
+
+for relation in ${relations[*]}; do
+    python3 filter_train.py  $relation &
+done
