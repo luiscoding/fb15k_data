3388c5ec78e32b012550e2c4e44fc08709d7e7b9
luiscoding <lu.zhang.code@gmail.com>
2020-04-01 14:18:40 -0400

    run_script

--- run_module.sh
+++ run_module.sh
@@ -1,10 +1,1 @@
-python3 extract_rule.py   concept_athletehomestadium &
-python3 extract_rule.py  concept_athleteplaysforteam &
-python3 extract_rule.py  concept_athleteplaysinleague &
-python3 extract_rule.py  concept_athleteplayssport &
-python3 extract_rule.py  concept_organizationhiredperson &
-python3 extract_rule.py concept_personborninlocation &
-python3 extract_rule.py concept_personleadsorganization &
-python3 extract_rule.py concept_teamplaysinleague &
-python3 extract_rule.py concept_teamplayssport &
-python3 extract_rule.py concept_worksfor &
+python filter_train.py
