60edf5115058af3b35ce46f75a780ad13a7fc64e
luiscoding <lu.zhang.code@gmail.com>
2020-05-07 14:31:48 -0400

    data too large

--- FB15k-237/run_tranX.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo $1
-relation=$1
-echo $relation
-# python transX.py $relation
-./transX -relation $relation
-echo "done"
-./eval_transX.py $relation
--- FB15k-237/test1.py
+++
@@ -1,5 +0,0 @@
-import numpy as np 
-
-M = np.loadtxt('A.bern')
-M = M.reshape([-1,100,100])
-print M[0,:,:].shape
